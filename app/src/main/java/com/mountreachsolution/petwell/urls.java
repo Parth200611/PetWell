c6055dde38a09e3bed8bb51468a6e1a2e3e2868c
Parth200611 <parthdahapute7@gmail.com>
2025-02-15 03:03:49 +0530

    commit

--- app/src/main/java/com/mountreachsolution/petwell/urls.java
+++ app/src/main/java/com/mountreachsolution/petwell/urls.java
@@ -1,8 +1,10 @@
 package com.mountreachsolution.petwell;
 
 public class urls {
-    public static String address="http://192.168.1.3:80/petwell/";
+    public static String address="http://192.168.1.2:80/petwell/";
     public static String userregister=address+"userregister.php";
     public static String petuserregister=address+"petRegister.php";
     public static String login=address+"login.php";
+    public static String profil=address+"profildata.php";
+    public static String profilimage=address+"profilimage.php";
 }
