7a0f0e414198c258e8f59e469e878c6c1912b539
Parth200611 <parthdahapute7@gmail.com>
2025-02-14 20:21:44 +0530

    commit

--- app/src/main/java/com/mountreachsolution/petwell/UserHomepage.java
+++ app/src/main/java/com/mountreachsolution/petwell/UserHomepage.java
@@ -1,24 +1,62 @@
 package com.mountreachsolution.petwell;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowInsetsController;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class UserHomepage extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class UserHomepage extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_homepage);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.orange)));
+        Window window = getWindow();
+        window.setStatusBarColor(getResources().getColor(R.color.orange));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false);
+            window.getInsetsController().setSystemBarsAppearance(0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+        }
+
+        getWindow().setNavigationBarColor(ContextCompat.getColor(UserHomepage.this,R.color.white));
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        bottomNavigationView = findViewById(R.id.bottomnevigatiomuserhome);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        bottomNavigationView.setSelectedItemId(R.id.Home);
+
+    }
+    userHome userHome1=new userHome();
+    UserProfil userProfil = new UserProfil();
+    AddActivity addActivity = new AddActivity();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.Home){
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome,userHome1).commit();
+        }else if(item.getItemId()==R.id.addactivigty){
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome,addActivity).commit();
+        } else if(item.getItemId()==R.id.profil){
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome,userProfil).commit();
+        }
+        return true;
     }
 }
